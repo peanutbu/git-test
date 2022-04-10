06455243b1514ce1f69be4a44130de464628d677
tony <570015735@qq.com>
2022-04-10 15:24:32 +0800

    first commit

---
+++ src/main/java/com/ljt/GitTest.java
@@ -0,0 +1,13 @@
+package com.ljt;
+
+/**
+ * @author
+ * @version 1.0.0
+ * @description :
+ * @date
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
